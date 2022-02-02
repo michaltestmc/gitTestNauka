34ef05d6aebadc5d408e8cfaff84dd5d515f5cad
michaltest <cryptoinfobit@gmail.com>
2022-02-02 10:19:47 +0100

    add Pętle

---
+++ src/PętlaDoWhile.java
@@ -0,0 +1,10 @@
+public class PętlaDoWhile {
+
+    public static void main(String[] args) {
+        int number = 21b;
+        do {
+            System.out.println("Moja wartość to: " + number + " i jestem mniejszy od 20");
+            number++;
+        } while (number < 20);
+    }
+}
---
+++ src/PętlaWhile.java
@@ -0,0 +1,9 @@
+public class PętlaWhile {
+    public static void main(String[] args) {
+        int number = 0;
+        while (number < 20) {
+            System.out.println("Moja wartość to: " + number + " i jestem mniejszy od 20");
+            number++;
+        }
+    }
+}
